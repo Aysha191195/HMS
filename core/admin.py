4d29ce14fe57389567dc08f722a12ced802c417d
Aysha Reshma <ayshareshma95@gmail.com>
2025-09-29 17:12:14 +0530

    create admin.py

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Doctor, Patient, Appointment, Bill
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Bill)
